1859012dc975aaf9227d7f52ed5b976c959aa372
Segeco <Segeco@users.noreply.github.com>
2021-01-17 12:40:53 +0000

    Add files via upload

---
+++ ebayviewbot.py
@@ -0,0 +1,26 @@
+import requests, colorama, os, time, json
+from colorama import Fore, Back, Style,init
+init()
+
+# Example
+# proxies = {'https': 'https://johnsmith:PASSWORD@66.78.45.72:2212'}
+
+proxies = {}
+
+def init():
+    print(Fore.CYAN + "eBay Viewbot v1.0" + Fore.WHITE)
+    eBayURL = input("eBay URL: ")
+    eBayViews = input("Amount of views: ")
+    os.system('cls')
+    print("Sending views to"+Fore.GREEN+" "+eBayURL + Fore.WHITE)
+    for i in range(int(eBayViews)):
+        if proxies == "":
+            rq = requests.get(eBayURL)
+            print(Fore.GREEN+"[Success] Sent view without using a proxy")
+        else:
+            rq = requests.get(eBayURL,proxies=proxies)
+            print(Fore.GREEN+"[Success] Sent view using a proxy")
+
+
+
+init()
